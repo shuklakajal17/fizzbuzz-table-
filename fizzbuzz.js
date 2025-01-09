fdb1455df8130c2a84f4dcf40844112f4ea5f219
shuklakajal17 <shuklakajal1703@gmail.com>
2025-01-09 21:17:54 +0530

    My initial JS code

---
+++ fizzbuzz.js
@@ -0,0 +1,11 @@
+let number = 4
+
+
+if (number == 3 ){
+    console.log("Kajal Shukla");
+
+} else if (number >3 ) {
+    console.log("Kajal");
+} else if (number <3 ) {
+    console.log("Shukla");
+}
